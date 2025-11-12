56107aa0fad7f4d2da956fe90fc73bd7c6b3f27c
s0rlesa <juan.alrosjua@gmail.com>
2025-11-11 18:41:51 -0600

    CreaTulanding+Rosales

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
   return (
     <>
       <div>
-        Saludos Cordiales
+        <NavBar></NavBar>
       </div>
       <div>
         {saludMensaje}
---
+++ src/components/layout/navbar/index.jsx
@@ -0,0 +1,12 @@
+const NavBar = () => {
+    <nav>
+        <ul>
+            <li><a href="">qw</a></li>
+            <li><a href="">we</a></li>
+            <li><a href="">er</a></li>
+            <li><a href="">rt</a></li>
+        </ul>
+    </nav>
+} 
+
+export default NavBar
